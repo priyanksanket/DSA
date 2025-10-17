2054a7e6735e260a34317f3bc768914a948449e3
Priyank Sanket <157965999+priyanksanket@users.noreply.github.com>
2025-10-17 23:05:00 +0530

    Time: 3 ms (32.76%) | Memory: 44.8 MB (79.03%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] numberGame(int[] nums) {
+        int n= nums.length;
+        int[] arr= new int[n];
+        Arrays.sort(nums);
+        for(int i=0;i<n;i+=2){
+            arr[i]=nums[i+1];
+            arr[i+1]=nums[i];
+        }
+        return arr;
+        
+    }
+}
