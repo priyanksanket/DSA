4f68052b08f4a3aa4f7add946e5ce2795d116ad5
Priyank Sanket <157965999+priyanksanket@users.noreply.github.com>
2025-10-20 09:56:23 +0530

    Time: 2 ms (76.18%) | Memory: 44.5 MB (96.89%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,27 +1,15 @@
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character,Integer> r= new HashMap<>();
-        Map<Character,Integer> m= new HashMap<>();
-        for(int i=0;i<ransomNote.length();i++){
-            char x= ransomNote.charAt(i);
-            if(r.containsKey(x)){
-                r.put(x,r.get(x)+1);
-            }else{
-                r.put(x,1);
-            }
+    public boolean canConstruct(String r, String m) {
+        
+        int []arr = new int[26];
+        for(char x:m.toCharArray()){
+            arr[x-'a']++;
         }
-        for(int i=0;i<magazine.length();i++){
-            char x= magazine.charAt(i);
-            if(m.containsKey(x)){
-                m.put(x,m.get(x)+1);
-            }else{
-                m.put(x,1);
-            }
+        for(char x:r.toCharArray()){
+            arr[x-'a']--;
         }
-        for(Map.Entry<Character,Integer>entry:r.entrySet()){
-            if(!m.containsKey(entry.getKey())) return false;
-            if(m.get(entry.getKey())<r.get(entry.getKey())) return false;
-
+        for(int i=0;i<26;i++){
+            if(arr[i]<0)return false;
         }
         return true;
         
