d793eb34b3ab46721562e4203469472db1cc35f4
Priyank Sanket <157965999+priyanksanket@users.noreply.github.com>
2025-10-29 13:15:04 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (53.84%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -1,49 +1,23 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// optimised solution without storing values in a list first;
 class Solution {
-    public TreeNode fill(List<Integer> l1,int l,int r){
-        if(l>r) return null;
-        int mid= l+(r-l)/2;
-        TreeNode root= new TreeNode(l1.get(mid));
-        root.left=fill(l1,l,mid-1);
-        root.right= fill(l1,mid+1,r);
-        return root;
-    }
     public TreeNode sortedListToBST(ListNode head) {
         if(head==null) return null;
-        List<Integer> l1= new ArrayList<>();
-        ListNode temp=head;
-        while(temp!= null){
-            l1.add(temp.val);
-            temp= temp.next;
+        if(head.next==null) return new TreeNode(head.val);
+        ListNode slow= head;
+        ListNode fast= head;
+        ListNode prev_slow= null;
+        while(fast!= null && fast.next!= null){
+            prev_slow= slow;
+            slow= slow.next;
+            fast= fast.next.next;
 
         }
-        
-        
+        prev_slow.next=null;
+        TreeNode root= new TreeNode(slow.val);
+        root.left=sortedListToBST(head);
+        root.right=sortedListToBST(slow.next);
+        return root;
 
-        return fill(l1,0,l1.size()-1);
+        
     }
 }
