4ed0eb3023e54cbb493e6500863ef255b0323ea2
Priyank Sanket <157965999+priyanksanket@users.noreply.github.com>
2026-06-16 21:29:25 +0530

    Time: 9 ms (42.29%) | Memory: 42.2 MB (15.89%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    // int [] dp;
+     int fun(int n){
+       if(n<=1) return n;
+        return  fun(n-1)+fun(n-2);
+    }
+
+    public int fib(int n) {
+        // dp=new int[n+1];
+
+        return fun(n);
+    }
+}
