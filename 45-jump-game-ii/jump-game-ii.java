fc734cc4eab0cd1d660184c23a4c7b38262eacd8
Priyank Sanket <157965999+priyanksanket@users.noreply.github.com>
2026-08-02 10:50:11 +0530

    Time: 1 ms (99.73%) | Memory: 47.5 MB (8.76%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int jumps = 0;
+        int l=0;
+        int r = 0;
+        while(r<n-1){
+            int farthest = 0;
+            for(int i = l;i<=r;i++){
+                farthest = Math.max(farthest, i+nums[i]);
+                
+            }
+            l = r +1;
+            r = farthest;
+            jumps++;
+        }
+        return jumps;
+        
+    }
+}
