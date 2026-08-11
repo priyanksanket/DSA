811c9562cca2bb85702e2962ff5a20cc00782be2
Priyank Sanket <157965999+priyanksanket@users.noreply.github.com>
2026-08-11 06:45:24 +0530

    Time: 18 ms (67.63%) | Memory: 108.3 MB (29.43%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> st = new HashSet<>();
+        for(int x: nums){
+            if(st.contains(x)) return true;
+            st.add(x);
+        }
+        return false;
+    }
+}
