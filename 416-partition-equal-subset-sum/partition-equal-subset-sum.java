caa7d7630af5cce3c31741b4b6dec001455fef56
Priyank Sanket <157965999+priyanksanket@users.noreply.github.com>
2026-08-14 04:03:15 +0530

    Time: 50 ms (47.78%) | Memory: 47.6 MB (49.81%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -14,9 +14,7 @@ class Solution {
         for(int i=0;i<n;i++){
             dp[i][0]=true;
         }
-        // if(nums[0]<=h){
-        //     dp[0][nums[0]]=true;
-        // }
+        
         
         
         for(int i=1;i<n;i++){
